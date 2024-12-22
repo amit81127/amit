4f9ed8c3c26c940c12a5668634e49de32166a1d1
Amitk kumar prasad <amitprasad8112@gmail.com>
2024-12-22 22:18:38 +0530

    Add files via upload

---
+++ potfolio.js
@@ -0,0 +1,14 @@
+// var typed= new typed(".text",{
+//     strings:["Web Developer","Gamer","Software Enginner"],
+//     typrSpeed:100,
+//     backSpeed:100,
+//     backDelay:1000,
+//     loop:true
+// });
+var typed = new Typed(".text", {
+    strings: ["Web Developer", "Gamer", "Software Engineer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
